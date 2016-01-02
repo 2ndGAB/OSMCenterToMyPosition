2b0b286eea846721bfa268f2aa69880950274184
Alain <alain.bonnefoy@cabb-online.com>
2016-01-02 16:37:33 +0100

    Add files

---
+++ app/src/main/java/com/example/Fragment_map.java
@@ -0,0 +1,97 @@
+package com.ndguide.ndguide;
+
+
+
+public class Fragment_welcome extends Fragment {
+
+    /*
+     * OSM
+     */
+    private MapView mMapView;
+    private MyLocationNewOverlay mLocationOverlay;
+    private CompassOverlay mCompassOverlay;
+    private MinimapOverlay mMinimapOverlay;
+    private ScaleBarOverlay mScaleBarOverlay;
+    private RotationGestureOverlay mRotationGestureOverlay;
+    private ResourceProxy mResourceProxy;
+    protected ImageButton btCenterMap;
+
+
+    public Fragment_welcome() {
+		// TODO Auto-generated constructor stub
+    }
+
+    public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		activity = (MainActivity)getActivity();
+
+		fm = getFragmentManager();
+		fragment_welcome = this;
+		
+		this.setRetainInstance(true);
+		
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+
+        mResourceProxy = new ResourceProxyImpl(inflater.getContext().getApplicationContext());
+        mMapView = (MapView) rootView.findViewById(R.id.mapview);
+
+        return rootView;
+	}
+	
+    @Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		//super.onViewCreated(view, savedInstanceState);
+
+        final Context context = this.getActivity();
+        final DisplayMetrics dm = context.getResources().getDisplayMetrics();
+
+        mPrefs = context.getSharedPreferences(Constants.OSM_PREFS, Context.MODE_PRIVATE);
+        mPrefsEditor = mPrefs.edit();
+
+        this.mCompassOverlay = new CompassOverlay(context, new InternalCompassOrientationProvider(context),
+                mMapView);
+        this.mLocationOverlay = new MyLocationNewOverlay(context, new GpsMyLocationProvider(context),
+                mMapView);
+
+        mScaleBarOverlay = new ScaleBarOverlay(context);
+        mScaleBarOverlay.setCentred(true);
+        mScaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
+
+        mRotationGestureOverlay = new RotationGestureOverlay(context, mMapView);
+        mRotationGestureOverlay.setEnabled(true);
+
+        mMapView.setTilesScaledToDpi(true);
+        mMapView.setBuiltInZoomControls(true);
+        mMapView.setMultiTouchControls(true);
+        mMapView.setFlingEnabled(true);
+        mMapView.getOverlays().add(this.mLocationOverlay);
+        mMapView.getOverlays().add(this.mCompassOverlay);
+        mMapView.getOverlays().add(this.mScaleBarOverlay);
+
+        mLocationOverlay.enableMyLocation();
+        mLocationOverlay.enableFollowLocation();
+        mLocationOverlay.setOptionsMenuEnabled(true);
+        mCompassOverlay.enableCompass();
+
+        site.setDisplayBoundingBox(mMapView.getBoundingBox());
+
+        btCenterMap = (ImageButton) view.findViewById(R.id.ic_center_map);
+
+        btCenterMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(Constants.APP_TAG, "centerMap clicked ");
+                GeoPoint myPosition = new GeoPoint(currentLocaion.getLatitude(), currentLocation.getLongitude());
+                mMapView.getController().animateTo(myPosition);
+            }
+        });
+
+    }
+}
